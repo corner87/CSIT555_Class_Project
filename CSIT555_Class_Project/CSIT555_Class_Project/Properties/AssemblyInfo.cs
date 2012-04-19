183452f1775efcfff8a317b2afed309f67c326b2
Ken Hofgesang <kgh4@hood.edu>
2012-04-19 19:55:01 -0400

    Edited Comment added 'A'

--- CSIT555_Class_Project/CSIT555_Class_Project/Properties/AssemblyInfo.cs
+++ CSIT555_Class_Project/CSIT555_Class_Project/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿/////////////////////////////////////////////////////////////////////////////////////////////////
 /// This file is part of CSIT 555 Digital Watermarks Demo.
 ///
-/// CSIT 555 Digital Watermarks Demo is free software: you can 
+/// CSIT 555 Digital Watermarks Demo is a free software: you can 
 /// redistribute it and/or modify it under the terms of the GNU 
 /// General Public License as published by the Free Software 
 /// Foundation, either version 3 of the License, or (at your 
